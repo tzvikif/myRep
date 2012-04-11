362141887a645a5ad62e9f00f9657bdc04946d27
tzviki <tzvikif@gmail.com>
2012-04-11 23:50:22 +0300

    xcode files

---
+++ testGit/testGit/FZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FZAppDelegate.h
+//  testGit
+//
+//  Created by tzviki fisher on 10/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testGit/testGit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testGit
+//
+//  Created by tzviki fisher on 10/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FZAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FZAppDelegate class]));
+    }
+}
