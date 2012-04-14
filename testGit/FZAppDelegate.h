21be5b004a12920d2670d85a96ec5406ed126131
tzviki <tzvikif@gmail.com>
2012-04-15 00:39:15 +0300

    go

---
+++ testGit/FZAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  FZAppDelegate.h
+//  testGit
+//
+//  Created by tzviki fisher on 10/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FZAppDelegate : UIResponder <UIApplicationDelegate>
+{
+    BOOL isOK;
+}
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
